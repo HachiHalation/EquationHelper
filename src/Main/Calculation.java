049523f92b3fce5e63cc91ecbd02b929a412c691
Matthew <matthewlf98@gmail.com>
2018-02-12 20:36:32 -0600

    Edited the Calculation File -Added a Hashmap -Rearragned comments

--- src/Main/Calculation.java
+++ src/Main/Calculation.java
@@ -3,9 +3,13 @@ import java.math.*;
 import java.util.*;
 
 public class Calculation {
+    //A Hash Map that will store the equations
+    //Name (The ID) will link to different equations
+    HashMap<String, Equation> equations = new HashMap<String, Equation>();
+
+
     //Method that takes in an ArrayList of strings and an Equation Object
     //The method will output a string with the input
-
     String input(ArrayList<String> userIn, Equation equObj){
         userIn = new ArrayList<>();
         equObj = new Equation();
@@ -14,19 +18,14 @@ public class Calculation {
     }
 
 
-
-    //A Hash Map that will store the equations
-    //Name (The ID) will link to different equations
-
     /*
         Method in Calculation: Takes in user input (parameters and equation object)
         > In the method, gives equation object user input
-        > equation sets its internal arraylist to user input
+        > equation sets its internal ArrayList to user input
         > equation rearranges based on what must be solved
         > calculation tells equation to solve for missing parameter
         > equation should have all parameters solved
         > calculation gets parameters from equation and outputs to interface
-
      */
 
 }
