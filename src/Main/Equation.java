63cd63e951b16cb9de72e72d4fce47361d114bb1
Trent Phan <phantrent@gmail.com>
2018-02-13 14:41:13 -0600

    added concrete elements to equation class

--- src/Main/Equation.java
+++ src/Main/Equation.java
@@ -7,15 +7,30 @@ public class Equation {
     /**
      * Takes in an arraylist of parameters and an arraylist of operations to perform
      */
-    ArrayList<Object> parameters;
-    ArrayList<Object> operations;
+    private ArrayList<Variable> parameters;
+    private ArrayList<Object> operations;
+    private char[] validOperations = {};
 
-    public Equation(){
+    public Equation(String s){
+        char[] temp = s.toCharArray();
+        for(char c: temp){
+            for(char op: validOperations){
+                if(op == c) operations.add(c);
+                else if(c != ' '){
+                    Variable var = new Variable(c, null);
+                    operations.add(var);
+                }
+            }
+        }
 
     }
 
-    public void setParameters(ArrayList<Object> parameters1){
-        parameters = new ArrayList<>(parameters1);
+    public void setParameters(ArrayList<Variable> parameters1){
+        for(Variable v: parameters1){
+            for(Variable param: parameters){
+                if(param.sameName(v)) param.setValue(v.value);
+            }
+        }
     }
 
     private void rearrange(){
@@ -23,10 +38,33 @@ public class Equation {
         //rearrange to isolate it
     }
 
-    public ArrayList<Object> solve(){
+    public ArrayList<Variable> solve(){
         //solve and return parameters
         return parameters;
     }
 
+    public class Variable{
+        private char name;
+        private Double value;
+
+        public Variable(char name, Double value){
+            this.name = name;
+            this.value = value;
+        }
+
+        public void setValue(Double d){
+            value = d;
+        }
+
+        public boolean sameName(Variable other){
+            if(other.name == name) return true;
+            else return false;
+        }
+
+        public String toString(){
+            return name + ": " + value;
+        }
+
+    }
 
 }
